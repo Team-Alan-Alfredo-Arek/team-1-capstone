ab1c853eb5d8b3cc92055c90a4537627ff7daa9f
Alfredoferreiras <Alfredoferreiras@yahoo.com>
2023-09-07 21:48:07 -0400

    styling on the event title

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { logout } from '../store';
-import { Navbar, Nav } from 'react-bootstrap';
+import React from "react";
+import { connect } from "react-redux";
+import { logout } from "../store";
+import { Navbar, Nav } from "react-bootstrap";
 
 const MyNavbar = ({ handleClick, isLoggedIn }) => (
-  <Navbar className="navbar gradient-custom" variant="light" expand="lg"> 
+  <Navbar className="navbar gradient-custom" variant="light" expand="lg">
     {/* Added gradient-custom to className */}
-    <Navbar.Brand href="#home">Capstone</Navbar.Brand>
+    <Navbar.Brand href="#home">PlanPerfect</Navbar.Brand>
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav">
       <Nav className="ml-auto">
@@ -15,7 +15,9 @@ const MyNavbar = ({ handleClick, isLoggedIn }) => (
             <Nav.Link href="/createevent">Create Event</Nav.Link>
             <Nav.Link href="/events">Events</Nav.Link>
             <Nav.Link href="/userprofile/:id">Profile</Nav.Link>
-            <Nav.Link href="#" onClick={handleClick}>Logout</Nav.Link>
+            <Nav.Link href="#" onClick={handleClick}>
+              Logout
+            </Nav.Link>
           </>
         ) : (
           <>
@@ -32,19 +34,18 @@ const MyNavbar = ({ handleClick, isLoggedIn }) => (
 /**
  * CONTAINER
  */
-const mapState = state => {
+const mapState = (state) => {
   return {
-    isLoggedIn: !!state.auth.id
+    isLoggedIn: !!state.auth.id,
   };
 };
 
-const mapDispatch = dispatch => {
+const mapDispatch = (dispatch) => {
   return {
     handleClick() {
       dispatch(logout());
-    }
+    },
   };
 };
 
 export default connect(mapState, mapDispatch)(MyNavbar);
-
