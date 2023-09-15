d3d3f95eaf0654d79e86e9c4c1e94429e9086a8f
Alfredoferreiras <Alfredoferreiras@yahoo.com>
2023-09-15 02:07:09 -0400

    Styling to chat

--- client/components/Chat.js
+++ client/components/Chat.js
@@ -3,6 +3,8 @@ import { useDispatch, useSelector } from "react-redux";
 import { sendMessage, fetchMessages, addNewMessageThunk } from "../store/chat";
 import { startListeningForMessages } from "../store/chat";
 import { useParams } from "react-router-dom";
+import { Container, Row, Col, Button } from "react-bootstrap";
+import { motion } from "framer-motion";
 import io from "socket.io-client";
 
 function ChatComponent() {
@@ -46,32 +48,87 @@ function ChatComponent() {
     }
   };
 
-  console.log("chat", chat);
+  const bodyStyle = {
+    fontFamily: "'Helvetica Neue', sans-serif",
+    backgroundColor: "#ffffff",
+    margin: 0,
+    padding: 0,
+    lineHeight: 1.6,
+    color: "#333",
+  };
+
+  const navbarStyle = {
+    backgroundColor: "#ffc71d",
+    padding: "1rem 0",
+  };
+
+  const gradientCustomStyle = {
+    background:
+      "linear-gradient(to right, rgba(250, 112, 154, 0.7), rgba(254, 225, 64, 0.7))",
+  };
+
+  const chatContainerStyle = {
+    display: "flex",
+    flexDirection: "column",
+    width: "400px",
+    height: "500px",
+    border: "1px solid #ccc",
+    borderRadius: "5px",
+    boxShadow: "0 4px 6px rgba(0, 0, 0, 0.1)",
+    overflow: "hidden",
+  };
+
+  const chatMessagesStyle = {
+    flexGrow: 1,
+    overflowY: "auto",
+    padding: "1rem",
+    ...gradientCustomStyle,
+  };
+
+  const chatInputStyle = {
+    display: "flex",
+    alignItems: "center",
+    padding: "1rem",
+    borderTop: "1px solid #eee",
+  };
+
+  const inputStyle = {
+    flexGrow: 1,
+    marginRight: "1rem",
+    padding: "0.5rem",
+    border: "1px solid #ccc",
+    borderRadius: "4px",
+  };
+
   return (
-    <div className="container mt-5">
-      <div className="list-group">
-        {chat?.map((message, index) => (
-          <div className="list-group-item" key={index}>
-            {message.message}
+    <Container style={bodyStyle}>
+      <Row className="justify-content-md-center">
+        <Col md="auto">
+          <div style={chatContainerStyle}>
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              style={chatMessagesStyle}>
+              {chat?.map((message, index) => (
+                <div key={index}>
+                  <strong>{message?.user?.username}: </strong>
+                  {message?.message}
+                </div>
+              ))}
+            </motion.div>
+            <div style={chatInputStyle}>
+              <input
+                style={inputStyle}
+                type="text"
+                value={inputMessage}
+                onChange={(e) => setInputMessage(e.target.value)}
+              />
+              <Button onClick={handleSendMessage}>Send</Button>
+            </div>
           </div>
-        ))}
-      </div>
-      <div className="input-group mt-3">
-        <input
-          type="text"
-          className="form-control"
-          value={inputMessage}
-          onChange={(e) => setInputMessage(e.target.value)}
-        />
-        <div className="input-group-append">
-          <button
-            className="btn btn-outline-primary"
-            onClick={handleSendMessage}>
-            Send
-          </button>
-        </div>
-      </div>
-    </div>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
